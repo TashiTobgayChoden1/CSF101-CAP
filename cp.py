a4b5443aa8d83421fa8dd6ba1998e480d1b0da35
User <02230228.cst@rub.edu.bt>
2024-04-24 14:18:28 +0100

    try

---
+++ cp.py
@@ -0,0 +1,13 @@
+#name: Tashi Tobgay Choden,
+ #section: 1ICE
+ #StudentIDNumber: 02230228
+ #REFERENCES:
+ #https://www.askpython.com/python/examples/easy-games-in-python
+ #https://www.youtube.com/watch?v=fn68QNcatfo
+ #https://www.youtube.com/watch?v=Gyc5dMdP2uc
+ #theproblem
+ #http://link.to.an.article/video.com
+ #SOLUTION:
+ #your solution score: 49843
+ #input_8_cap1
+
