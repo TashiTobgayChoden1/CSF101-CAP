f510575a714c64849665dca2e3eecf2867e75687
User <02230228.cst@rub.edu.bt>
2024-04-24 14:31:48 +0100

    cap1

--- CAP1_02230228.py
+++ CAP1_02230228.py
@@ -1,3 +1,15 @@
+#name: Tashi Tobgay Choden,
+ #section: 1ICE
+ #StudentIDNumber: 02230228
+ #REFERENCES:
+ #https://www.askpython.com/python/examples/easy-games-in-python
+ #https://www.youtube.com/watch?v=fn68QNcatfo
+ #https://www.youtube.com/watch?v=Gyc5dMdP2uc
+ #theproblem
+ #http://link.to.an.article/video.com
+ #SOLUTION:
+ #your solution score: 49843
+ #input_8_cap1
 def compute_round_score(opponent_move, desired_outcome):
     move_scores = {'A': 1, 'B': 2, 'C': 3}
     outcome_scores = {'X': 0, 'Y': 3, 'Z': 6}
